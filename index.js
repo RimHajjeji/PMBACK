98fe17e99f471250503f0b9b763d521fdef44e29
RimHajjeji <rimhajjeji210@gmail.com>
2024-08-19 10:32:44 +0100

    commit2

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
-const multer = require('multer');
-const app = express(); // Use express() to create an app instance
+const app = express(); 
 
 app.use(cors());
+app.use(express.json());
 
 mongoose.connect("mongodb+srv://admin:admin12345678@cluster0.awqce.mongodb.net/PM", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+app.use("/api/admin", require("./routes/admin"));
 
 app.listen(5000, () => {
     console.log("Server is Running on port 5000!!!");
---
+++ routes/admin.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Admin = require("../models/Admin");
+
+const router = express.Router();
+
+// Admin login route
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const admin = await Admin.findOne({ email });
+        if (!admin) {
+            return res.status(400).json({ msg: "Invalid credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, admin.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ id: admin._id }, "yourJWTSecret", {
+            expiresIn: 3600, // 1 hour
+        });
+
+        res.json({ token });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
+
+module.exports = router;
